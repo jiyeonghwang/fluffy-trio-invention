eea8d7ca59818e78453a5840f73ea5dc6ce880cf
limetree-j <limetree.jdh@gmail.com>
2022-01-23 00:05:50 +0900

    [STUDY] 도형 문제 풀이 업데이트

---
+++ HackerRank/DoHyung/SolutionMockTest02DH.java
@@ -0,0 +1,26 @@
+import java.util.List;
+
+public class SolutionMockTest02DH {
+    class Result {
+        /*
+         * Complete the 'flippingMatrix' function below.
+         *
+         * The function is expected to return an INTEGER.
+         * The function accepts 2D_INTEGER_ARRAY matrix as parameter.
+         */
+        public static int flippingMatrix(List<List<Integer>> matrix) {
+            int answer = 0;
+            int n = matrix.size();
+            for (int i=0;i<n/2;i++) {
+                for (int j=0;j<n/2;j++) {
+                    answer += Math.max(matrix.get(i).get(j),
+                                Math.max(matrix.get(i).get(n-1-j),
+                                    Math.max(matrix.get(n-1-i).get(j), matrix.get(n-1-i).get(n-1-j))));
+                }
+            }
+
+            return answer;
+        }
+    }
+
+}
