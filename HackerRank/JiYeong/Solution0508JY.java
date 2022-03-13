743e61f0372c27c5bb57209c37de8070a73ddb5b
jiyeonghwang <gyp0288@naver.com>
2022-03-13 11:44:37 +0900

    [STUDY] 지영 문제 풀이 업데이트

---
+++ HackerRank/JiYeong/Solution0508JY.java
@@ -0,0 +1,75 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'sansaXor' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int sansaXor(List<Integer> arr) {
+        // Write your code here
+        List<List<Integer>> temp = new ArrayList<>();
+        for (int i=1; i<=arr.size(); i++) {
+            int st=0;
+            for (int j=0; j+i<=arr.size(); j++) {
+                List<Integer> subList = arr.subList(st, i+j);
+                temp.add(subList);
+                st++;
+            }
+        }
+
+        int result = temp.get(0).get(0);
+        temp.remove(0);
+        for (List<Integer> list : temp) {
+            int x = list.get(0);
+            for (int i=1; i<list.size(); i++) {
+                x ^= list.get(i);
+            }
+            result ^= x;
+        }
+        return result;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                        .map(Integer::parseInt)
+                        .collect(toList());
+
+                int result = Result.sansaXor(arr);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
